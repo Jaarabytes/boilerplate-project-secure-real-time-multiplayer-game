4513c444d424ef09c9c78ee5d2ef0f9c9d7db49b
Jaarabytes <xavierandole78@gmail.com>
2025-03-13 16:12:21 +0300

    i don't know
    only added some security features yet all tests pass. Will code
    the whole game later

--- server.js
+++ server.js
@@ -1,38 +1,48 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const expect = require('chai');
-const socket = require('socket.io');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const expect = require("chai");
+const socket = require("socket.io");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const fccTestingRoutes = require('./routes/fcctesting.js');
-const runner = require('./test-runner.js');
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const runner = require("./test-runner.js");
 
 const app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/assets', express.static(process.cwd() + '/assets'));
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use("/assets", express.static(process.cwd() + "/assets"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Mitigate the risk of Cross Site Scripting (XSS) attacks
+app.use(helmet.xssFilter());
+
+// Hide the X-Powered-By header
+app.use(helmet.hidePoweredBy({ setTo: "PHP 7.4.3" }));
+
+// Disable client-side caching
+app.use(helmet.noCache());
+
+// Avoid inferring the response MIME type
+app.use(helmet.noSniff());
+
 //For FCC testing purposes and enables user to connect from outside the hosting platform
-app.use(cors({origin: '*'})); 
+app.use(cors({ origin: "*" }));
 
 // Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  }); 
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
-    
+
 // 404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use(function (req, res, next) {
+  res.status(404).type("text").send("Not Found");
 });
 
 const portNum = process.env.PORT || 3000;
@@ -40,13 +50,13 @@ const portNum = process.env.PORT || 3000;
 // Set up server and tests
 const server = app.listen(portNum, () => {
   console.log(`Listening on port ${portNum}`);
-  if (process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
     setTimeout(function () {
       try {
         runner.run();
       } catch (error) {
-        console.log('Tests are not valid:');
+        console.log("Tests are not valid:");
         console.error(error);
       }
     }, 1500);
